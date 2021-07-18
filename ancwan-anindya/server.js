fe51c63bdb8f550ee251393e33df09073fbb9b5a
hadlinar <hadlinar@gmail.com>
2021-07-18 23:36:10 +0700

    purge cache

--- ancwan-anindya/server.js
+++ ancwan-anindya/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
 const app = express();
-app.use(favicon(__dirname + '/build/anindya.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
