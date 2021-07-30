a496f0df2eed3ce02215a74ae503e11dc9288033
hadlinar <hadlinar@gmail.com>
2021-07-30 18:14:02 +0700

    done contact us

--- ancwan-anindya/src/components/pages/contact us/ContactUs.js
+++ ancwan-anindya/src/components/pages/contact us/ContactUs.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Offices from './Offices'
+import FormMessage from './FormMessage'
 
 function ContactUs() {
     return (
         <>
             <Offices />
+            <FormMessage />
         </>
     )
 }
