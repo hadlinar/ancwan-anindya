1ccf9b7612d921aa8e54c87e9700d286d9c65ace
hadlinar <hadlinar@gmail.com>
2021-09-04 16:33:51 +0700

    fixed certification web ver

--- ancwan-anindya/src/components/pages/our company/Licenses.js
+++ ancwan-anindya/src/components/pages/our company/Licenses.js
@@ -13,106 +13,109 @@ function Licenses() {
                 <div className="title-license">{t('licenses')}</div>
                 <p className="title-desc">{t('licenses_desc')}</p>
                 <div>
-                    <Tab.Container id="left-tabs-example" defaultActiveKey={1}>
-                        <Row  style={{backgroundColor: "#E0E0E0", border: "1px solid #E0E0E0"}}>
-                            <Col sm={4} style={{paddingLeft: "0", paddingRight: "0"}}>
-                                <Nav variant="pills" className="flex-column">
-                                    <Nav.Item>
+                    <Tab.Container defaultActiveKey={1}>
+                        <Row>
+                            <Col sm={4} className='left-col'>
+                                <Nav variant="pills" className="nav-left">
+                                    <Nav.Item className='nav_pills'>
                                         <Nav.Link eventKey={1} className="name-tab">{t('licenses_tab1.name')}</Nav.Link>
                                     </Nav.Item>
-                                    <Nav.Item>
+                                    <Nav.Item className='nav_pills'>
                                         <Nav.Link eventKey={2} className="name-tab">{t('licenses_tab2.name')}</Nav.Link>
                                     </Nav.Item>
-                                    <Nav.Item>
+                                    <Nav.Item className='nav_pills'>
                                         <Nav.Link eventKey={3} className="name-tab">{t('licenses_tab3.name')}</Nav.Link>
                                     </Nav.Item>
                                 </Nav>
                             </Col>
-                            <Col sm={8} style={{paddingLeft: "0", paddingRight: "0"}}>
-                            <Tab.Content className="license-content">
-                                <Tab.Pane eventKey={1}>
-                                    <div style={{fontWeight: "600"}}>{t('licenses_tab1.title')}</div>
-                                    <ul className="list-content">
-                                        {
-                                            Array.from(data.licenses_tab1.val, (e, i) => {
-                                            return(
-                                                <li style={{paddingTop: "3px"}}>{t(`licenses_tab1.val.${i}`)}</li>
-                                            )
-                                        })}
-                                    </ul>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey={2}>
-                                <div style={{fontWeight: "600"}}>{t('licenses_tab2.title')}</div>
-                                    <ul className="list-content">
-                                        {
-                                            Array.from(data.licenses_tab2.val, (e, i) => {
-                                            return(
-                                                <li style={{paddingTop: "3px"}}>{t(`licenses_tab2.val.${i}`)}</li>
-                                            )
-                                        })}
-                                    </ul>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey={3}>
-                                <div style={{fontWeight: "600"}}>{t('licenses_tab3.title')}</div>
-                                    <ul className="list-content">
-                                        {
-                                            Array.from(data.licenses_tab3.val, (e, i) => {
-                                            return(
-                                                <li style={{paddingTop: "3px"}}>{t(`licenses_tab3.val.${i}`)}</li>
-                                            )
-                                        })}
-                                    </ul>
-                                </Tab.Pane>
-                            </Tab.Content>
+                            <Col sm={8} className='right-col'>
+                                <Tab.Content className='content-right'>
+                                    <Tab.Pane eventKey={1}>
+                                        <div style={{fontWeight: "600"}}>{t('licenses_tab1.title')}</div>
+                                        <ul className="list-content">
+                                            {
+                                                Array.from(data.licenses_tab1.val, (e, i) => {
+                                                return(
+                                                    <li style={{paddingTop: "3px"}}>{t(`licenses_tab1.val.${i}`)}</li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </Tab.Pane>
+                                    <Tab.Pane eventKey={2}>
+                                        <div style={{fontWeight: "600"}}>{t('licenses_tab2.title')}</div>
+                                        <ul className="list-content">
+                                            {
+                                                Array.from(data.licenses_tab2.val, (e, i) => {
+                                                return(
+                                                    <li style={{paddingTop: "3px"}}>{t(`licenses_tab2.val.${i}`)}</li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </Tab.Pane>
+                                    <Tab.Pane eventKey={3}>
+                                        <div style={{fontWeight: "600"}}>{t('licenses_tab3.title')}</div>
+                                        <ul className="list-content">
+                                            {
+                                                Array.from(data.licenses_tab3.val, (e, i) => {
+                                                return(
+                                                    <li style={{paddingTop: "3px"}}>{t(`licenses_tab3.val.${i}`)}</li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </Tab.Pane>
+                                </Tab.Content>
                             </Col>
                         </Row>
                     </Tab.Container>
                 </div>
+            </div>
+
+            <div style={{paddingTop: '80px'}}>
                 <div className="title-license">{t('certification')}</div>
                 <p className="title-desc">{t('certification_desc')}</p>
                 <div>
-                <Tab.Container id="left-tabs-example" defaultActiveKey={1}>
-                        <Row  style={{backgroundColor: "#E0E0E0", border: "1px solid #E0E0E0"}}>
-                            <Col sm={4} style={{paddingLeft: "0", paddingRight: "0"}}>
-                                <Nav variant="pills" className="flex-column">
-                                    <Nav.Item>
-                                        <Nav.Link eventKey={1} className="certif-tab">{t('certification_tab1.name')}</Nav.Link>
+                    <Tab.Container defaultActiveKey={1}>
+                        <Row>
+                            <Col sm={4} className='left-col-certif'>
+                                <Nav variant="pills" className="nav-left">
+                                    <Nav.Item className='nav_pills_certif'>
+                                        <Nav.Link eventKey={1} className="name-tab-certif">{t('certification_tab1.name')}</Nav.Link>
                                     </Nav.Item>
-                                    <Nav.Item>
-                                        <Nav.Link eventKey={2} className="certif-tab">{t('certification_tab2.name')}</Nav.Link>
+                                    <Nav.Item className='nav_pills_certif'>
+                                        <Nav.Link eventKey={2} className="name-tab-certif">{t('certification_tab2.name')}</Nav.Link>
                                     </Nav.Item>
                                 </Nav>
                             </Col>
-                            <Col sm={8} style={{paddingLeft: "0", paddingRight: "0"}}>
-                            <Tab.Content className="certif-content">
-                                <Tab.Pane eventKey={1}>
-                                    {
-                                        Array.from(data.certification_tab1.desc, (e, i) => {
-                                        return(
-                                            <>
-                                                <div style={{fontWeight: "600"}}>{t(`certification_tab1.desc.${i}.no`)}</div>
-                                                <p className="certif-desc">{t(`certification_tab1.desc.${i}.detail`)}</p>
-                                            </>
-                                        )
-                                    })}
+                            <Col sm={8} className='right-col'>
+                                <Tab.Content className='content-right'>
+                                    <Tab.Pane eventKey={1}>
+                                        {
+                                            Array.from(data.certification_tab1.desc, (e, i) => {
+                                            return(
+                                                <>
+                                                    <div style={{fontWeight: "600"}}>{t(`certification_tab1.desc.${i}.no`)}</div>
+                                                    <p className="certif-desc">{t(`certification_tab1.desc.${i}.detail`)}</p>
+                                                </>
+                                            )
+                                        })}
                                         { Array.from(data.certification_tab1.val, (e, i) => {
                                             return(
                                                 <p className="certif-list ">{t(`certification_tab1.val.${i}`)}</p>
                                             )
                                         })}
-                                </Tab.Pane>
-                                <Tab.Pane eventKey={2}>
-                                {
-                                        Array.from(data.certification_tab2.desc, (e, i) => {
-                                        return(
-                                            <>
-                                                <div style={{fontWeight: "600"}}>{t(`certification_tab2.desc.${i}.no`)}</div>
-                                                <p className="certif-desc">{t(`certification_tab2.desc.${i}.detail`)} <br/> {t(`certification_tab2.desc.${i}.detail2`)}</p>
-                                            </>
-                                        )
-                                    })}
-                                </Tab.Pane>
-                            </Tab.Content>
+                                    </Tab.Pane>
+                                    <Tab.Pane eventKey={2}>
+                                        {
+                                            Array.from(data.certification_tab2.desc, (e, i) => {
+                                            return(
+                                                <>
+                                                    <div style={{fontWeight: "600"}}>{t(`certification_tab2.desc.${i}.no`)}</div>
+                                                    <p className="certif-desc">{t(`certification_tab2.desc.${i}.detail`)} <br/> {t(`certification_tab2.desc.${i}.detail2`)}</p>
+                                                </>
+                                            )
+                                        })}
+                                    </Tab.Pane>
+                                </Tab.Content>
                             </Col>
                         </Row>
                     </Tab.Container>
