ba5778fd53d014cb5556fb06174a02292ebaaf93
hilyafesmia <hilyafesmia@gmail.com>
2023-10-19 02:41:37 +0700

    add desc section

---
+++ src/components/pages/tkdn/Desc.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Desc.css";
+import { useTranslation } from "react-i18next";
+
+function Desc() {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <div className="container-desc-tkdn">
+        <p className="about-tkdn">{t("desc_tkdn")}</p>
+      </div>
+    </>
+  );
+}
+
+export default Desc;
