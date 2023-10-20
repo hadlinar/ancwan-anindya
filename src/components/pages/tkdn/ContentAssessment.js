97e326a63e88928e78adf27456784e3df839dbda
hilyafesmia <hilyafesmia@gmail.com>
2023-10-20 15:51:08 +0700

    add mobile support to content assessment section

--- src/components/pages/tkdn/ContentAssessment.js
+++ src/components/pages/tkdn/ContentAssessment.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Definition.css";
 import "./ContentAssessment.css";
 import { useTranslation } from "react-i18next";
