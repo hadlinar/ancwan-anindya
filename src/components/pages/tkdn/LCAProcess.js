7dbc676cd2d1fe7d4100e2a0e9722ef2be8c8e3f
hilyafesmia <hilyafesmia@gmail.com>
2023-10-20 15:52:35 +0700

    add mobile support to LCA process section

--- src/components/pages/tkdn/LCAProcess.js
+++ src/components/pages/tkdn/LCAProcess.js
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./Definition.css";
 import { useTranslation } from "react-i18next";
-import lcaProcessImg from "../../../images/TKDN/LCA Verification Process.png";
 
 function LCAProcess() {
   const { t } = useTranslation();
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    mobileVer();
+  }, []);
+
+  const mobileVer = () => {
+    if (window.innerWidth <= 960) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  };
+
+  window.addEventListener("resize", mobileVer);
 
   return (
     <>
@@ -13,7 +27,18 @@ function LCAProcess() {
           <div className="border-title-vpti" />
           <h1 className="tkdn-section-title">{t("lca_process_title")}</h1>
         </div>
-        <img src={lcaProcessImg} />
+        <img
+          src={
+            mobile
+              ? require(`../../../images/TKDN/LCA Process-Mobile-${t(
+                  "image_language"
+                )}.png`).default
+              : require(`../../../images/TKDN/LCA Process-Web-${t(
+                  "image_language"
+                )}.png`).default
+          }
+          alt={t("lca_process_title")}
+        />
       </div>
     </>
   );
