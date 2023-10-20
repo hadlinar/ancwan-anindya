e8c026aa9c1acf88e6f3b2090ed57cdaa814ede3
hilyafesmia <hilyafesmia@gmail.com>
2023-10-20 15:50:22 +0700

    add mobile support to form section

--- src/components/pages/tkdn/ComplainForm.js
+++ src/components/pages/tkdn/ComplainForm.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from "react";
 import "./Definition.css";
+import "./ComplainForm.css";
 import { useTranslation } from "react-i18next";
-import { Button, ButtonVPTI } from "../../Button";
+import { Button } from "../../Button";
 import { Row, Col, Form } from "react-bootstrap";
 
 function ComplainForm() {
   const { t } = useTranslation();
+  const [mobile, setMobile] = useState(false);
 
+  useEffect(() => {
+    mobileVer();
+  }, []);
+
+  const mobileVer = () => {
+    if (window.innerWidth <= 960) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  };
+
+  window.addEventListener("resize", mobileVer);
   return (
     <>
       <div className="tkdn-container-definition">
@@ -17,31 +32,40 @@ function ComplainForm() {
           </h1>
         </div>
         <Form>
-          <Row>
-            <Col sm={4}>
+          {mobile ? (
+            <Row className="form-container">
               <Form.Group>
                 <Form.Label>{t("submission_type_form")}</Form.Label>
-                <Row>
+                <Row className="submission-type-container">
                   {["complaint", "appeal"].map((type) => (
-                    <Form.Check type={"radio"} id={type} label={type} />
+                    <Form.Check
+                      type={"radio"}
+                      name="typePicker"
+                      id={type}
+                      label={type}
+                    />
                   ))}
                 </Row>
               </Form.Group>
-              <Form.Group>
+              <Form.Group className="input-container">
                 <Form.Label>{t("company_name_form")}</Form.Label>
                 <Form.Control as="input" />
               </Form.Group>
-              <Form.Group>
+              <Form.Group className="input-container">
                 <Form.Label>{t("company_pic_form")}</Form.Label>
                 <Form.Control as="input" />
               </Form.Group>
-              <Form.Group>
+              <Form.Group className="input-container">
                 <Form.Label>{t("company_address_form")}</Form.Label>
                 <Form.Control as="input" />
               </Form.Group>
-              <Form.Group>
+              <Form.Group className="input-container">
                 <Form.Label>{t("company_phone_form")}</Form.Label>
                 <Form.Control as="input" type="number" />
+              </Form.Group>
+              <Form.Group className="input-container">
+                <Form.Label>{t("details_form")}</Form.Label>
+                <Form.Control as="textarea" rows={15} />
               </Form.Group>
               <Button
                 buttonStyle="btn--primary"
@@ -50,14 +74,55 @@ function ComplainForm() {
               >
                 {t("button_form_submit")}
               </Button>
-            </Col>
-            <Col sm={8}>
-              <Form.Group>
-                <Form.Label>{t("company_name_form")}</Form.Label>
-                <Form.Control as="textarea" rows={15} />
-              </Form.Group>
-            </Col>
-          </Row>
+            </Row>
+          ) : (
+            <Row>
+              <Col sm={4} className="form-container">
+                <Form.Group>
+                  <Form.Label>{t("submission_type_form")}</Form.Label>
+                  <Row className="submission-type-container">
+                    {["Complaint", "Appeal"].map((type) => (
+                      <Form.Check
+                        type={"radio"}
+                        name="typePicker"
+                        id={type}
+                        label={type}
+                      />
+                    ))}
+                  </Row>
+                </Form.Group>
+                <Form.Group className="input-container">
+                  <Form.Label>{t("company_name_form")}</Form.Label>
+                  <Form.Control as="input" />
+                </Form.Group>
+                <Form.Group className="input-container">
+                  <Form.Label>{t("company_pic_form")}</Form.Label>
+                  <Form.Control as="input" />
+                </Form.Group>
+                <Form.Group className="input-container">
+                  <Form.Label>{t("company_address_form")}</Form.Label>
+                  <Form.Control as="input" />
+                </Form.Group>
+                <Form.Group className="input-container">
+                  <Form.Label>{t("company_phone_form")}</Form.Label>
+                  <Form.Control as="input" type="number" />
+                </Form.Group>
+                <Button
+                  buttonStyle="btn--primary"
+                  buttonSize="btn--xsmall"
+                  type="submit"
+                >
+                  {t("button_form_submit")}
+                </Button>
+              </Col>
+              <Col sm={8}>
+                <Form.Group>
+                  <Form.Label>{t("details_form")}</Form.Label>
+                  <Form.Control as="textarea" rows={15} />
+                </Form.Group>
+              </Col>
+            </Row>
+          )}
         </Form>
       </div>
     </>
