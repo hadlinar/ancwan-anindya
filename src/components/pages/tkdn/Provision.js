c22ac633366e89392dc1b39f5563353c8af7a994
hilyafesmia <hilyafesmia@gmail.com>
2023-10-20 15:50:33 +0700

    add mobile support to provision section

--- src/components/pages/tkdn/Provision.js
+++ src/components/pages/tkdn/Provision.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Definition.css";
 import "./Provision.css";
 import { useTranslation } from "react-i18next";
