cd40d16e8ed1f842b4a22bebf3dc3c1c2c2f16c2
hilyafesmia <hilyafesmia@gmail.com>
2023-10-20 15:48:55 +0700

    add chevron icon to faq card with mui

--- src/components/pages/tkdn/FAQ.js
+++ src/components/pages/tkdn/FAQ.js
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Definition.css";
 import "./FAQ.css";
 import { useTranslation } from "react-i18next";
-import data from "../../../data";
 import Accordion from "react-bootstrap/Accordion";
+import Icon from "@mdi/react";
+import { mdiChevronDown } from "@mdi/js";
 
 function FAQ() {
   const { t } = useTranslation();
+
+  const odd = [1, 3, 5, 7, 9];
+  const even = [0, 2, 4, 6, 8];
 
   return (
     <>
@@ -16,21 +20,33 @@ function FAQ() {
           <h1 className="tkdn-section-title">{t("faq_title")}</h1>
         </div>
         <div>
-          <Accordion className="faq-list" defaultActiveKey={0} flush>
-            {Array.from(data.faq_list, (faq, i) => {
-              return (
-                <Accordion.Item eventKey={i} key={i}>
-                  <Accordion.Header className="faq-card">
+          <Accordion className="faq-list">
+            <div className="faq-half">
+              {even.map((i) => (
+                <Accordion.Item eventKey={i} key={i} className="faq-card">
+                  <Accordion.Header className="faq-header">
                     <h3 className="tkdn-subtitle-2">
                       {t(`faq_list.${i}.title`)}
                     </h3>
+                    <Icon path={mdiChevronDown} size={1} />
                   </Accordion.Header>
-                  <Accordion.Body className="faq-card">
-                    {t(`faq_list.${i}.desc`)}
-                  </Accordion.Body>
+                  <Accordion.Body>{t(`faq_list.${i}.desc`)}</Accordion.Body>
                 </Accordion.Item>
-              );
-            })}
+              ))}
+            </div>
+            <div className="faq-half">
+              {odd.map((i) => (
+                <Accordion.Item eventKey={i} key={i} className="faq-card">
+                  <Accordion.Header className="faq-header">
+                    <h3 className="tkdn-subtitle-2">
+                      {t(`faq_list.${i}.title`)}
+                    </h3>
+                    <Icon path={mdiChevronDown} size={1} />
+                  </Accordion.Header>
+                  <Accordion.Body>{t(`faq_list.${i}.desc`)}</Accordion.Body>
+                </Accordion.Item>
+              ))}
+            </div>
           </Accordion>
         </div>
       </div>
