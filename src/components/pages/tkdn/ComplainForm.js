e7689c81b7aaddc42d7178a4f1c6fffb3268a2e5
hilyafesmia <hilyafesmia@gmail.com>
2023-11-13 11:23:03 +0700

    remove unecessary states in complainform.js

--- src/components/pages/tkdn/ComplainForm.js
+++ src/components/pages/tkdn/ComplainForm.js
@@ -1,71 +1,10 @@
-import React, { useState, useEffect } from "react";
 import "./Definition.css";
 import "./ComplainForm.css";
 import { useTranslation } from "react-i18next";
-import { Button } from "../../Button";
-import { Row, Col, Form } from "react-bootstrap";
 
 function ComplainForm() {
   const { t } = useTranslation();
-  const [mobile, setMobile] = useState(false);
 
-  const [submissionType, setSubmissionType] = useState("");
-  const [companyName, setCompanyName] = useState("");
-  const [companyPic, setCompanyPic] = useState("");
-  const [companyAddress, setCompanyAddress] = useState("");
-  const [companyPhone, setCompanyPhone] = useState("");
-  const [detailSubmission, setDetailSubmission] = useState("");
-
-  useEffect(() => {
-    mobileVer();
-  }, []);
-
-  const mobileVer = () => {
-    if (window.innerWidth <= 960) {
-      setMobile(true);
-    } else {
-      setMobile(false);
-    }
-  };
-
-  const handleSubmissionType = (e) => {
-    setSubmissionType(e.target.id);
-  };
-
-  const handleName = (e) => {
-    setCompanyName(e.target.value);
-  };
-
-  const handlePic = (e) => {
-    setCompanyPic(e.target.value);
-  };
-
-  const handleAdress = (e) => {
-    setCompanyAddress(e.target.value);
-  };
-
-  const handlePhone = (e) => {
-    setCompanyPhone(e.target.value);
-  };
-
-  const handleDetail = (e) => {
-    setDetailSubmission(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    const dataToSend = {
-      submissionType: submissionType,
-      companyName: companyName,
-      companyPic: companyPic,
-      companyAddress: companyAddress,
-      companyPhone: companyPhone,
-      detailSubmission: detailSubmission,
-    };
-
-    console.log(JSON.stringify(dataToSend));
-  };
-
-  window.addEventListener("resize", mobileVer);
   return (
     <>
       <div className="tkdn-container-definition" id="complain-form">
